25a0294519b0324471813a18e0fdbb0782fc9617
hansama0902 <42849277+hansama0902@users.noreply.github.com>
2024-10-21 20:18:10 -0700

    Update create_table.sql

--- Query/create_table.sql
+++ Query/create_table.sql
@@ -1,86 +1,109 @@
-CREATE TABLE PatientManagementAPP (
-    patient_id INTEGER PRIMARY KEY,
+CREATE TABLE Patient (
+    patient_id INTEGER,
     first_name TEXT,
     last_name TEXT,
     phone INTEGER,
     DOB DATE,
     address TEXT,
-    gender TEXT CHECK(gender IN ('male', 'female', 'other'))
+    gender TEXT CHECK(gender IN ('male', 'female', 'other')),
+    PRIMARY KEY (patient_id)
 );
-CREATE TABLE HealthSurveillance (
-    survey_id INTEGER PRIMARY KEY,
+CREATE TABLE Survey (
+    survey_id INTEGER,
     last_sync DATETIME,
     symptom TEXT,
-    FOREIGN KEY (survey_id) REFERENCES PatientManagementAPP(patient_id)
+    immuno_compromised BOOLEAN,
+    patient_id INTEGER,
+    PRIMARY KEY (survey_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id)
 );
 CREATE TABLE DiseasesHistory (
     survey_id INTEGER,
     diseases_name TEXT,
     PRIMARY KEY (survey_id),
-    FOREIGN KEY (survey_id) REFERENCES HealthSurveillance(survey_id)
+    FOREIGN KEY (survey_id) REFERENCES Survey(survey_id)
 );
+
 CREATE TABLE Test (
-    test_id INTEGER PRIMARY KEY,
+    test_id INTEGER,
     test_type TEXT CHECK(test_type IN ('CBC', 'Antibody', 'CRP')),
     test_date DATETIME,
-    survey_id INTEGER,
+    patient_id INTEGER,
     lab_id INTEGER,
-    FOREIGN KEY (survey_id) REFERENCES HealthSurveillance(survey_id),
+    PRIMARY KEY (test_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
     FOREIGN KEY (lab_id) REFERENCES Lab(lab_id)
 );
+
 CREATE TABLE Lab (
-    lab_id INTEGER PRIMARY KEY,
+    lab_id INTEGER,
     lab_name TEXT,
-    zipcode TEXT
+    zipcode TEXT,
+    PRIMARY KEY(lab_id)
 );
+
+CREATE TABLE Appointment (
+    appointment_id INTEGER,
+    appointment_date DATETIME,
+    patient_id INTEGER,
+    doctor_id INTEGER,
+    PRIMARY KEY(appointment_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id)
+);
+
 CREATE TABLE LabReport (
-    report_id INTEGER PRIMARY KEY,
+    report_id INTEGER,
     illness_name TEXT,
     memo TEXT,
     lab_id INTEGER,
     doctor_id INTEGER,
+    PRIMARY KEY(report_id),
     FOREIGN KEY (lab_id) REFERENCES Lab(lab_id),
     FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id)
 );
+
 CREATE TABLE Doctor (
-    doctor_id INTEGER PRIMARY KEY,
+    doctor_id INTEGER,
     doctor_name TEXT,
     license_number INTEGER,
-    phone INTEGER
+    phone INTEGER,
+    PRIMARY KEY(doctor_id)
 );
+
 CREATE TABLE Prescription (
-    prescription_id INTEGER PRIMARY KEY,
-    date_issued TEXT,
+    prescription_id INTEGER,
+    date_issued DATETIME,
     dosage_instructions TEXT,
     doctor_id INTEGER,
+    PRIMARY KEY(prescription_id),
     FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id)
 );
+
 CREATE TABLE Medicine (
-    medicine_id INTEGER PRIMARY KEY,
+    medicine_id INTEGER,
     medicine_name TEXT,
     dosage_frequency TEXT,
     prescription_id INTEGER,
-    monitor_id INTEGER,
-    FOREIGN KEY (prescription_id) REFERENCES Prescription(prescription_id),
-    FOREIGN KEY (monitor_id) REFERENCES MedicineMonitor(monitor_id)
+    PRIMARY KEY(medicine_id),
+    FOREIGN KEY (prescription_id) REFERENCES Prescription(prescription_id)
 );
-CREATE TABLE MedicineMonitor (
-    monitor_id INTEGER PRIMARY KEY,
-    monitoring_date DATE,
-    monitoring_status TEXT,
-    FOREIGN KEY (monitor_id) REFERENCES PatientManagementAPP(patient_id)
-);
-CREATE TABLE Delivery (
-    delivery_id INTEGER PRIMARY KEY,
-    delivery_date TEXT,
-    address INTEGER,
-    phone INTEGER
-);
-CREATE TABLE DeliveryTracer (
-    delivery_id INTEGER,
+
+CREATE TABLE MedicineRecord (
+    record_id INTEGER,
+    medication_issuance_frequency INTEGER,
+    medication_issuance_time DATETIME,
+    patient_id INTEGER,
     medicine_id INTEGER,
-    PRIMARY KEY (delivery_id, medicine_id),
-    FOREIGN KEY (delivery_id) REFERENCES Delivery(delivery_id),
+    PRIMARY KEY(record_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
     FOREIGN KEY (medicine_id) REFERENCES Medicine(medicine_id)
 );
 
+CREATE TABLE DeliveryTracer (
+    patient_id INTEGER,
+    medicine_id INTEGER,
+    PRIMARY KEY (patient_id, medicine_id),
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
+    FOREIGN KEY (medicine_id) REFERENCES Medicine(medicine_id)
+);
