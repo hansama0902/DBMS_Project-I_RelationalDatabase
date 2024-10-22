03ac20da184496f009846a5d7f405ea47b3f0aaa
hansama0902 <42849277+hansama0902@users.noreply.github.com>
2024-10-21 19:39:19 -0700

    Update query1.sql

--- Query/query1.sql
+++ Query/query1.sql
@@ -1,12 +1,12 @@
 SELECT
-    PatientManagementAPP.first_name,
-    PatientManagementAPP.last_name,
+    Patient.first_name,
+    Patient.last_name,
     Test.test_type,
     Lab.lab_name
 FROM
-    PatientManagementAPP
-JOIN HealthSurveillance ON PatientManagementAPP.patient_id = HealthSurveillance.survey_id
-JOIN Test ON HealthSurveillance.survey_id = Test.survey_id
+    Patient
+JOIN Survey ON Patient.patient_id = Survey.patient_id
+JOIN Test ON Patient.patient_id = Test.patient_id
 JOIN Lab ON Test.lab_id = Lab.lab_id;
 -- The SQL statement retrieves information about patients, including their first name, last name, the type of test they underwent,
 -- and the name of the laboratory that conducted the test.
