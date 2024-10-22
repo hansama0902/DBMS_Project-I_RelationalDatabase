6b7081383c03027e1da34862ab614e685e97f2e7
hansama0902 <42849277+hansama0902@users.noreply.github.com>
2024-10-21 19:53:00 -0700

    Update query5.sql

--- Query/query5.sql
+++ Query/query5.sql
@@ -1,9 +1,13 @@
 SELECT
-    doctor_id,
-    prescription_id,
-    date_issued,
-    COUNT(*) OVER (PARTITION BY doctor_id) AS total_prescriptions_per_doctor
+    Patient.patient_id,
+    Patient.first_name,
+    Patient.last_name,
+    Test.test_type,
+    Test.test_date,
+    RANK() OVER (PARTITION BY Patient.patient_id ORDER BY Test.test_date DESC) AS test_rank
 FROM
-    Prescription;
--- The SQL statement retrieves information about prescriptions, including the doctor_id, prescription_id,
--- and date_issued. It also calculates the total number of prescriptions issued by each doctor.
+    Patient
+JOIN Survey ON Patient.patient_id = Survey.patient_id
+JOIN Test ON Patient.patient_id = Test.patient_id
+
+-- Rank each patient's tests based on test date, with the most recent test ranked as 1.
